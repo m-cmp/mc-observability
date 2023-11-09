2cddbc076bdeb56f93a70d8497368196156c77d1
KyeonghyeonKim <134241403+hyeon-inno@users.noreply.github.com>
2023-11-09 15:34:22 +0900

    init

---
+++ src/main/java/mcmp.mc.observability.agent/AgentApplication.java
@@ -0,0 +1,11 @@
+package mcmp.mc.observability.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentApplication.class, args);
+    }
+}
