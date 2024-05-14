3e527c644c83afeb664c964b9f8540248eed2caf
KyeonghyeonKim <134241403+hyeon-inno@users.noreply.github.com>
2024-05-14 12:39:09 +0900

    Resolve Conflict

--- java/src/main/java/mcmp/mc/observability/agent/AgentApplication.java
+++ java/src/main/java/mcmp/mc/observability/agent/AgentApplication.java
@@ -3,6 +3,7 @@ package mcmp.mc.observability.agent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableScheduling
 @SpringBootApplication
