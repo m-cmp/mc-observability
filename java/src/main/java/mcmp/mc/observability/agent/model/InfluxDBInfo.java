a82275a12f9080f5c05e5a731acdbd2f6eb8f2c5
KyeonghyeonKim <134241403+hyeon-inno@users.noreply.github.com>
2024-05-25 20:48:20 +0900

    Update swagger.yaml

--- java/src/main/java/mcmp/mc/observability/agent/model/InfluxDBInfo.java
+++ java/src/main/java/mcmp/mc/observability/agent/model/InfluxDBInfo.java
@@ -18,6 +18,20 @@ public class InfluxDBInfo {
     private String password;
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        InfluxDBInfo that = (InfluxDBInfo) o;
+
+        if (!Objects.equals(url, that.url)) return false;
+        if (!Objects.equals(database, that.database)) return false;
+        if (!Objects.equals(retentionPolicy, that.retentionPolicy)) return false;
+        if (!Objects.equals(username, that.username)) return false;
+        return Objects.equals(password, that.password);
+    }
+
+    @Override
     public int hashCode() {
         return Objects.hash(url, database, retentionPolicy, username, password);
     }
