ba82979ed94c18851d16af710893363a32b87dd5
kyuengmanKim <mytoby@naver.com>
2024-09-03 16:05:21 +0900

    feat: API description UPDATE

--- python/insight/app/api/anomaly/anomaly.py
+++ python/insight/app/api/anomaly/anomaly.py
@@ -12,6 +12,9 @@ router = APIRouter()
 
 @router.get("/anomaly-detection/options", response_model=ResBodyAnomalyDetectionOptions)
 async def get_available_options_for_anomaly_detection():
+    """
+    Fetch the available target types, metric types, and interval options for the anomaly detection API.
+    """
     config_data = read_config("config/anomaly.ini")
 
     response = ResBodyAnomalyDetectionOptions(data=AnomalyDetectionOptions(**config_data))
@@ -21,6 +24,9 @@ async def get_available_options_for_anomaly_detection():
 
 @router.get("/anomaly-detection/settings", response_model=ResBodyAnomalyDetectionSettings)
 async def get_all_anomaly_detection_settings(db: Session = Depends(get_db)):
+    """
+    Fetch the current settings for all anomaly detection targets.
+    """
     anomaly_setting_service = AnomalySettingsService(db=db)
     response = anomaly_setting_service.get_all_settings()
     return response
@@ -28,23 +34,35 @@ async def get_all_anomaly_detection_settings(db: Session = Depends(get_db)):
 
 @router.post("/anomaly-detection/settings", response_model=ResBodyVoid)
 async def register_anomaly_detection_target(body: AnomalyDetectionTargetRegistration, db: Session = Depends(get_db)):
+    """
+    Register a target for anomaly detection and automatically schedule detection tasks.
+    """
     service = AnomalySettingsService(db=db)
     return service.create_setting(setting_data=body.dict())
 
 
 @router.put("/anomaly-detection/settings/{settingSeq}", response_model=ResBodyVoid)
 async def update_anomaly_detection_target(settingSeq: int, body: AnomalyDetectionTargetUpdate, db: Session = Depends(get_db)):
+    """
+    Modify the settings for a specific anomaly detection target, including the monitoring metric and interval.
+    """
     service = AnomalySettingsService(db=db)
     return service.update_setting(setting_seq=settingSeq, update_data=body.dict())
 
 
 @router.delete("/anomaly-detection/settings/{settingSeq}", response_model=ResBodyVoid)
 async def delete_anomaly_detection_target(settingSeq: int, db: Session = Depends(get_db)):
+    """
+    Remove a target from anomaly detection, stopping and removing any scheduled tasks.
+    """
     service = AnomalySettingsService(db=db)
     return service.delete_setting(setting_seq=settingSeq)
 
 
 @router.get("/anomaly-detection/settings/nsId/{nsId}/target/{targetId}", response_model=ResBodyAnomalyDetectionSettings)
 async def get_specific_anomaly_detection_target(nsId: str, targetId: str, db: Session = Depends(get_db)):
+    """
+    Fetch the current settings for a specific anomaly detection target.
+    """
     service = AnomalySettingsService(db=db)
     return service.get_setting(ns_id=nsId, target_id=targetId)
