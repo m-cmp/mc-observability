eeacbe656c095d28fdbba3a70151871e3a91345c
KyeonghyeonKim <134241403+hyeon-inno@users.noreply.github.com>
2024-09-11 11:23:56 +0900

    Add object base64encode/decode

--- java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/monitoring/controller/TargetController.java
+++ java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/monitoring/controller/TargetController.java
@@ -1,6 +1,8 @@
 package mcmp.mc.observability.mco11yagent.monitoring.controller;
 
 import lombok.RequiredArgsConstructor;
+import mcmp.mc.observability.mco11yagent.monitoring.annotation.Base64Decode;
+import mcmp.mc.observability.mco11yagent.monitoring.annotation.Base64Encode;
 import mcmp.mc.observability.mco11yagent.monitoring.common.Constants;
 import mcmp.mc.observability.mco11yagent.monitoring.model.TargetInfo;
 import mcmp.mc.observability.mco11yagent.monitoring.model.dto.ResBody;
@@ -23,21 +25,25 @@ public class TargetController {
 
     private final TargetService targetService;
 
+    @Base64Encode
     @GetMapping("/target")
     public ResBody<List<TargetInfo>> list() {
         return targetService.getList();
     }
 
+    @Base64Encode
     @GetMapping("/{nsId}/{mciId}/target/{targetId}")
     public ResBody<TargetInfo> getTarget(@PathVariable String nsId, @PathVariable String mciId, @PathVariable String targetId) {
         return targetService.getTarget(nsId, mciId, targetId);
     }
 
+    @Base64Decode(TargetInfo.class)
     @PostMapping("/{nsId}/{mciId}/target/{targetId}")
     public ResBody insert(@PathVariable String nsId, @PathVariable String mciId, @PathVariable String targetId, @RequestBody TargetInfo targetInfo) {
         return targetService.insert(nsId, mciId, targetId, targetInfo);
     }
 
+    @Base64Decode(TargetInfo.class)
     @PutMapping("/{nsId}/{mciId}/target/{targetId}")
     public ResBody update(@PathVariable String nsId, @PathVariable String mciId, @PathVariable String targetId, @RequestBody TargetInfo targetInfo) {
         return targetService.update(nsId, mciId, targetId, targetInfo);
--- java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/monitoring/model/TargetInfo.java
+++ java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/monitoring/model/TargetInfo.java
@@ -2,6 +2,8 @@ package mcmp.mc.observability.mco11yagent.monitoring.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import mcmp.mc.observability.mco11yagent.monitoring.annotation.Base64DecodeField;
+import mcmp.mc.observability.mco11yagent.monitoring.annotation.Base64EncodeField;
 
 @Getter
 @Setter
@@ -10,7 +12,11 @@ public class TargetInfo {
     private String mciId;
     private String id;
     private String name;
+    @Base64EncodeField
+    @Base64DecodeField
     private String aliasName;
+    @Base64EncodeField
+    @Base64DecodeField
     private String description;
     private String state;
 }
