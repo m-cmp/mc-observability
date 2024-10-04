da0550e0a5c02ae34691a5dc9a35552c8a1343b8
kyuengmanKim <mytoby@naver.com>
2024-10-04 11:58:48 +0900

    refactor: metric_type update 5

--- python/insight/app/api/anomaly/utils/setting.py
+++ python/insight/app/api/anomaly/utils/setting.py
@@ -18,7 +18,7 @@ class AnomalySettingsService:
                 nsId=setting.NAMESPACE_ID,
                 targetId=setting.TARGET_ID,
                 target_type=setting.TARGET_TYPE,
-                measurements=setting.MEASUREMENT,
+                measurement=setting.MEASUREMENT,
                 execution_interval=setting.EXECUTION_INTERVAL,
                 last_execution=setting.LAST_EXECUTION,
                 createAt=setting.REGDATE
