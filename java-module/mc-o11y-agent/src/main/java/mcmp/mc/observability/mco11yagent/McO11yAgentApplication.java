c8219a44d9b1d56114dfa3b4edf024da49ee017e
ish <ish@innogrid.com>
2024-10-12 13:24:07 +0900

    Implement swagger doc auto generation and add Makefile

--- java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/McO11yAgentApplication.java
+++ java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/McO11yAgentApplication.java
@@ -6,14 +6,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
 @MapperScan
 @EnableScheduling
 @EnableFeignClients(basePackages = "mcmp.mc.observability.mco11yagent")
 @SpringBootApplication
 public class McO11yAgentApplication {
-
     public static void main(String[] args) {
+        String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+
+        String pidFilePath = "mc-o11y-agent.pid";
+
+        try (FileWriter fileWriter = new FileWriter(pidFilePath)) {
+            fileWriter.write(pid);
+            System.out.println("PID file created: " + pidFilePath);
+        } catch (IOException e) {
+            System.err.println("Error creating PID file: " + e.getMessage());
+        }
+
         SpringApplication.run(McO11yAgentApplication.class, args);
     }
-
 }
--- java-module/mc-o11y-manager/src/main/java/mcmp/mc/observability/mco11ymanager/McO11yManagerApplication.java
+++ java-module/mc-o11y-manager/src/main/java/mcmp/mc/observability/mco11ymanager/McO11yManagerApplication.java
@@ -4,12 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
 @EnableFeignClients
 @SpringBootApplication
 public class McO11yManagerApplication {
-
     public static void main(String[] args) {
+        String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+
+        String pidFilePath = "mc-o11y-manager.pid";
+
+        try (FileWriter fileWriter = new FileWriter(pidFilePath)) {
+            fileWriter.write(pid);
+            System.out.println("PID file created: " + pidFilePath);
+        } catch (IOException e) {
+            System.err.println("Error creating PID file: " + e.getMessage());
+        }
+
         SpringApplication.run(McO11yManagerApplication.class, args);
     }
-
 }
