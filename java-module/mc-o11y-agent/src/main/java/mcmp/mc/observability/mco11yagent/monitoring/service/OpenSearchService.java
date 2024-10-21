b1ef6f72995b4643d7c1b3ef666322f861f42a47
ish <ish@innogrid.com>
2024-10-21 14:27:06 +0900

    java-module: Fix log searching

--- java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/monitoring/service/OpenSearchService.java
+++ java-module/mc-o11y-agent/src/main/java/mcmp/mc/observability/mco11yagent/monitoring/service/OpenSearchService.java
@@ -96,17 +96,14 @@ public class OpenSearchService {
                         "target_id", "tag.target_id"
                 );
 
-//             if( logsInfo.getConditions() != null ) logsInfo.getConditions().forEach(f -> boolQueryBuilder.must(QueryBuilders.matchQuery(f.getKey(), f.getValue())));
                 if (logsInfo.getConditions() != null) {
                     logsInfo.getConditions().forEach(condition -> {
                         String key = condition.getKey();
                         String value = condition.getValue();
 
-                        // key에 해당하는 필드명을 찾음. 없으면 key 그대로 사용
-                        String searchField = fieldMapping.getOrDefault(key, key);
+                        value = fieldMapping.getOrDefault(key, value);
 
-                        // 검색 쿼리 생성
-                        boolQueryBuilder.must(QueryBuilders.matchQuery(searchField, value));
+                        boolQueryBuilder.must(QueryBuilders.matchQuery(key, value));
                     });
                 }
 
