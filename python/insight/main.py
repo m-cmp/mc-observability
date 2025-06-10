72942154161ff8316c3256ac75c9e1511faca55b
inhun <torihome1@gmail.com>
2025-06-10 16:42:27 +0900

    feat: add initial structure for log analysis
    (cherry picked from commit 4f088146c2ba5c7c6481ddee5b15808edff24b6d)

--- python/insight/main.py
+++ python/insight/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.anomaly import anomaly
 from app.api.prediction import prediction
+from app.api.log_analysis import log_analysis
 from config.ConfigManager import ConfigManager
 import uvicorn
 
@@ -26,7 +27,7 @@ app.add_middleware(
 api_prefix = config.get_prefix()
 app.include_router(anomaly.router, prefix=api_prefix, tags=["[Insight] Anomaly Detection"])
 app.include_router(prediction.router, prefix=api_prefix, tags=["[Insight] Prediction"])
-
+app.include_router(log_analysis.router, prefix=api_prefix, tags=["[Insight] Log Analysis"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=9001, log_config="config/log.ini")
