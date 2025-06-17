63eb667b10c1a82aa30aa7418065e93efb2debff
inhun <torihome1@gmail.com>
2025-06-17 09:21:27 +0900

    feat: add get chat session history

---
+++ python/insight/app/api/log_analysis/utils/utils.py
@@ -0,0 +1,60 @@
+from config.ConfigManager import ConfigManager
+from app.api.log_analysis.response.res import LogAnalysisModel
+from app.api.log_analysis.repo.repo import LogAnalysisRepository
+from app.api.log_analysis.response.res import LogAnalysisSession
+
+from sqlalchemy.orm import Session
+
+import os
+import uuid
+
+
+class LogAnalysisService:
+    def __init__(self, db: Session):
+        self.repo = LogAnalysisRepository(db=db)
+        pass
+
+    def get_model_list(self, model_info_config):
+        result = []
+        for model_info in model_info_config:
+            if model_info['provider'] == 'ollama':
+                ollama_base_url = os.getenv('OLLAMA_BASE_URL', None)
+                if ollama_base_url:
+                    result.append(self.model_info_to_res(model_info))
+
+            elif model_info['provider'] == 'openai':
+                openai_api_key = os.getenv('OPENAI_API_KEY', None)
+                if openai_api_key:
+                    result.append(self.model_info_to_res(model_info))
+
+        return result
+
+    @staticmethod
+    def model_info_to_res(model_info):
+        return LogAnalysisModel(
+            provider=model_info['provider'],
+            model_name=model_info['model_name']
+        )
+
+    def get_chat_session(self):
+        sessions = self.repo.get_all_sessions()
+
+        results = [
+            LogAnalysisSession(
+                seq=session.SEQ,
+                user_id=session.USER_ID,
+                session_id=session.SESSION_ID,
+                provider=session.PROVIDER,
+                model_name=session.MODEL_NAME,
+                regdate=session.REGDATE
+            )
+            for session in sessions
+        ]
+
+
+        print(results)
+        return results
+
+
+    def create_session(self):
+        session_id = uuid.uuid4()
