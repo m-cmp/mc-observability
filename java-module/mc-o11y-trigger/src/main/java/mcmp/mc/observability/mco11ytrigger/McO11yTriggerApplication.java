a5547c532d02b39683e42dc340963a50e15c52e8
임재호 <jaeho_lim@innogrid.com>
2025-07-07 16:54:28 +0900

    init mc-o11y-trigger module

---
+++ java-module/mc-o11y-trigger/src/main/java/mcmp/mc/observability/mco11ytrigger/McO11yTriggerApplication.java
@@ -0,0 +1,13 @@
+package mcmp.mc.observability.mco11ytrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McO11yTriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McO11yTriggerApplication.class, args);
+	}
+
+}
---
+++ java-module/mc-o11y-trigger/src/test/java/mcmp/mc/observability/mco11ytrigger/McO11yTriggerApplicationTests.java
@@ -0,0 +1,13 @@
+package mcmp.mc.observability.mco11ytrigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McO11yTriggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
