4ac251fd064f29ccc6a7bd35aa0daa57f9e891fe
suahlingo <sgua0720@gmail.com>
2025-07-16 15:29:18 +0900

    feat : add spider port

---
+++ java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/dto/SpiderMonitoringInfo.java
@@ -0,0 +1,31 @@
+package com.mcmp.o11ymanager.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class SpiderMonitoringInfo {
+
+  @Getter
+  @Setter
+  @NoArgsConstructor
+  public static class Data {
+
+    private String metricName;
+    private String metricUnit;
+    private List<TimestampValue> timestampValues;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class TimestampValue {
+
+      private String timestamp;
+      private String value;
+    }
+  }
+}
