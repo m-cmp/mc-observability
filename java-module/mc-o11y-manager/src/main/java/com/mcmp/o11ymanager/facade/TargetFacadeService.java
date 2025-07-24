7bb3e8c36e7660b30151aa5529ae97aac661275f
suahlingo <sgua0720@gmail.com>
2025-07-24 16:58:59 +0900

    refactor : add targetStatus Running

--- java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/facade/TargetFacadeService.java
+++ java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/facade/TargetFacadeService.java
@@ -1,16 +1,13 @@
 package com.mcmp.o11ymanager.facade;
 
 
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mcmp.o11ymanager.dto.target.TargetDTO;
 import com.mcmp.o11ymanager.dto.target.TargetRegisterDTO;
 import com.mcmp.o11ymanager.dto.target.TargetUpdateDTO;
-import com.mcmp.o11ymanager.dto.tumblebug.TumblebugCmd;
 import com.mcmp.o11ymanager.dto.tumblebug.TumblebugMCI;
-import com.mcmp.o11ymanager.port.TumblebugPort;
+import com.mcmp.o11ymanager.model.host.TargetStatus;
 import com.mcmp.o11ymanager.service.interfaces.TargetService;
+import com.mcmp.o11ymanager.service.interfaces.TumblebugService;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
@@ -23,48 +20,25 @@ import org.springframework.stereotype.Service;
 public class TargetFacadeService {
 
   private final TargetService targetService;
-  private final TumblebugPort tumblebugPort;
   private final AgentFacadeService agentFacadeService;
-  private final ObjectMapper objectMapper;
+  private final TumblebugService tumblebugService;
 
-
-  public String executeCommand(String nsId, String mciId, String command, String userName) {
-    TumblebugCmd cmd = new TumblebugCmd();
-    cmd.setCommand(List.of(command));
-    cmd.setUserName(userName);
-
-    Object response = tumblebugPort.sendCommand(nsId, mciId, cmd);
-
-    try {
-      JsonNode root = objectMapper.valueToTree(response);
-      JsonNode results = root.path("results");
-      if (results.isArray() && results.size() > 0) {
-        JsonNode stdout = results.get(0).path("stdout");
-        return stdout.path("0").asText();
-      }
-    } catch (Exception e) {
-      throw new RuntimeException("Tumblebug 응답 파싱 실패: " + e.getMessage(), e);
-    }
-
-    throw new RuntimeException("Tumblebug 응답이 유효하지 않습니다.");
-  }
 
   public TargetDTO postTarget(String nsId, String mciId, String targetId, TargetRegisterDTO dto) {
 
-
     String output;
     try {
-      output = executeCommand(nsId, mciId, "echo hello", "cb-user");
+      output = tumblebugService.executeCommand(nsId, mciId, "echo hello", "cb-user");
 
       if (!"hello".equals(output.trim())) {
         throw new RuntimeException("Response is not hello. Actual Response: '" + output + "'");
       }
-
     } catch (Exception e) {
       throw new RuntimeException("Connection Error: " + e.getMessage(), e);
     }
 
     TargetDTO savedTarget = targetService.post(nsId, mciId, targetId, dto);
+    savedTarget.setTargetStatus(TargetStatus.RUNNING);
 
     agentFacadeService.install(nsId, mciId, targetId);
 
@@ -72,7 +46,7 @@ public class TargetFacadeService {
   }
 
   public TargetDTO getTarget(String nsId, String mciId, String targetId) {
-    TumblebugMCI.Vm vm = tumblebugPort.getVM(nsId, mciId, targetId);
+    TumblebugMCI.Vm vm = tumblebugService.getVm(nsId, mciId, targetId);
 
     return TargetDTO.builder()
         .targetId(vm.getId())
@@ -84,6 +58,7 @@ public class TargetFacadeService {
         .state(vm.getState())
         .build();
   }
+
 
   public List<TargetDTO> getTargetsNsMci(String nsId, String mciId) {
     return targetService.getByNsMci(nsId, mciId);
