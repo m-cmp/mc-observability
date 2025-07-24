72a096383daf93ee39bfe9e084b802a3819f3a7f
suahlingo <sgua0720@gmail.com>
2025-07-24 17:17:43 +0900

    refactor : remove unused field

--- java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/model/host/TargetAgentTaskStatus.java
+++ java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/model/host/TargetAgentTaskStatus.java
@@ -8,11 +8,8 @@ public enum TargetAgentTaskStatus {
     INSTALLING("Agent 설치가 진행 중 입니다."),
     UPDATING("Agent 업데이트가 진행 중 입니다."),
     UPDATING_CONFIG("Agent 설정 업데이트가 진행 중 입니다."),
-    ROLLING_BACK_CONFIG("Agent 설정 롤백이 진행 중 입니다."),
     UNINSTALLING("Agent 제거가 진행 중 입니다."),
     RESTARTING("Agent 재시작이 진행 중 입니다."),
-    ENABLING("Agent 활성화가 진행 중 입니다."),
-    DISABLING("Agent 비활성화가 진행 중 입니다."),
     IDLE("현재 진행중인 에이전트 관련 작업이 없습니다.");
 
     private final String message;
