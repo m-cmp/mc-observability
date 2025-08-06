1392a583c28406a725e7686f3885826cff7a1cdd
ish <55735188+ish-hcc@users.noreply.github.com>
2025-08-06 16:36:56 +0900

    Update init.sh

--- java-module/scripts/init.sh
+++ java-module/scripts/init.sh
@@ -27,7 +27,7 @@ if [ $? -ne 0 ]; then
     $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 git --version
