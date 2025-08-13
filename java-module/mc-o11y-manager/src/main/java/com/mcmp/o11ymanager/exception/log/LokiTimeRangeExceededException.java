d767a04125839c5878516642824aaa35cc54c97e
최낙수 <cns@innogrid.com>
2025-08-13 17:38:31 +0900

    refactor: Fix Loki error message

--- java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/exception/log/LokiTimeRangeExceededException.java
+++ java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/exception/log/LokiTimeRangeExceededException.java
@@ -9,14 +9,12 @@ import lombok.Getter;
  */
 @Getter
 public class LokiTimeRangeExceededException extends BaseException {
-    
     private final String queryLength;
     private final String limit;
-    
+
     public LokiTimeRangeExceededException(String requestId, String queryLength, String limit) {
         super(requestId, ErrorCode.LOKI_TIME_RANGE_EXCEEDED,
-            String.format("조회 시간 범위가 Loki 서버의 제한을 초과했습니다. (요청 기간: %s, 최대 허용 기간: %s) 더 짧은 시간 범위로 다시 시도해주세요.", 
-                queryLength, limit));
+                "조회 시간 범위가 Loki 서버의 제한을 초과했습니다.  더 짧은 시간 범위로 다시 시도해주세요.");
         this.queryLength = queryLength;
         this.limit = limit;
     }
