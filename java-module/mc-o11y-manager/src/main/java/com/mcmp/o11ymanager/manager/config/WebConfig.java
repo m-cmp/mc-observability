28fadb75d126bf0a99da783f8e2b68f858d3c24d
suahlingo <sgua0720@gmail.com>
2025-10-01 09:30:05 +0900

    refactor : refactor webconfig

--- java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/manager/config/WebConfig.java
+++ java-module/mc-o11y-manager/src/main/java/com/mcmp/o11ymanager/manager/config/WebConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -59,5 +60,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/api/docs").setViewName("forward:/swagger-ui/index.html");
     }
 }
