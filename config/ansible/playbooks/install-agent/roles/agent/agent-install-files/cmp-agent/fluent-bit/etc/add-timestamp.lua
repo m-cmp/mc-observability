7532dc91bda32664e2a0156c449e8a098bf4668f
Kyeongman Kim <mytoby@naver.com>
2025-10-21 11:41:20 +0900

    refactor: Updates docker compose & config dir 4

--- config/ansible/playbooks/install-agent/roles/agent/agent-install-files/cmp-agent/fluent-bit/etc/add-timestamp.lua
+++
@@ -1,9 +0,0 @@
-function add_timestamp(tag, timestamp, record)
-    local time_offset = os.getenv("TIME_DIFF_SECONDS") or "+0"
-
-    local value = tonumber(time_offset) or 0
-
-    timestamp = os.time() + value
-    
-    return 1, timestamp, record
-end
