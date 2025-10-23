84e7433f74e60fb4035922aa73f253bcbc560a0f
Kyeongman Kim <mytoby@naver.com>
2025-10-23 11:56:28 +0900

    refactor: delete unused file

--- java/log-conf/extract_component.lua
+++
@@ -1,6 +0,0 @@
-function extract_component(tag, timestamp, record)
-         if record["attributes"] and record["attributes"]["component"] then
-             record["component"] = record["attributes"]["component"]
-         end
-         return 1, timestamp, record
-     end
